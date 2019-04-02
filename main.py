cbc7f9866aa2cff3e781572676b68dece579b6ee
Guido <glambertucci@itba.edu.ar>
2019-04-02 13:22:00 -0300

    hola

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import metodos as Mt
 
 yuyin = Mt.Metodos(0.1)
+yuyin.calculate()
 yuyin.show_graph()
--- metodos.py
+++ metodos.py
@@ -6,34 +6,40 @@ from scipy.linalg import toeplitz
 
 class Metodos:
 
-    def __init__(self, dev):#standard deviation
-        self.L = 5
-        self.gain = 0.1
-        self.h = self.gain * (1 + np.random.randn(self.L, 1))
+    def __init__(self, dev):
         self.dev = dev
+#       standard deviation
+        self.a = plt.imread("lena512.bmp")
+#       loads the pic
+        self.b = 0
 
-        self.a = plt.imread("lena512.bmp")#loads the pic
+    def calculate(self):
+        l = 5
+        gain = 0.1
+        h = gain * (1 + np.random.randn(l, 1))
+#       col and fil size
+        m = len(self.a[:, 1])
+        p = len(self.a[1, :])
 
-        #col and fil size
-        self.M = len(self.a[:, 1])
-        self.P = len(self.a[1, :])
-
-        self.H = toeplitz(np.concatenate((np.transpose(self.h), np.zeros(self.M-self.L)), axis=None), np.zeros(self.M)) #Impulse response
-        self.N = self.dev*np.random.randn(self.M, self.P)#Noise
-        self.s = self.a.astype(float)#input
-
-        self.r = np.zeros((self.M, self.P))
-        self.r = (self.H.dot(self.s))+self.N
-        self.b = self.r.astype(int) #output
-
-        plt.figure(1)
-        plt.subplot(1,2,1)#loads the plots
-        plt.imshow(self.a,cmap='gray',vmin=0,vmax=255)
-        plt.subplot(1,2,2)
-        plt.imshow(self.b,cmap='gray',vmin=0,vmax=255)
+        H = toeplitz(np.concatenate((np.transpose(h), np.zeros(m-l)), axis=None), np.zeros(m))
+#       Impulse response
+        n = self.dev*np.random.randn(m, p)
+#       Noise
+        s = self.a.astype(float)
+#       input
+        r = (H.dot(s))+n
+        self.b = r.astype(int)
+#       output
 
     def show_graph(self):
+        plt.figure(1)
+        plt.subplot(1, 2, 1)
+#       loads the plots
+        plt.imshow(self.a, cmap='gray', vmin=0, vmax=255)
+        plt.subplot(1, 2, 2)
+        plt.imshow(self.b, cmap='gray', vmin=0, vmax=255)
         plt.show()
 
-    def singular_values(self, ent):#ent Training 2define
+    def singular_values(self, ent):
+#       ent Training 2define
         pass
