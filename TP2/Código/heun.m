0f89cc640ff811388a86c76d278b12d408ed244a
Tomás Guillermo Londero Bonaparte <tlondero@itba.edu.ar>
2019-06-01 15:47:30 -0300

    PUSHEO RE MANIJA PAPÁ

--- TP2/Código/heun.m
+++ TP2/Código/heun.m
@@ -3,9 +3,9 @@ function [t, x]=heun(f,x0,t0,tf,n,par)
 	t=linspace(t0,tf,n+1);
 	x(1,:)=x0;
 	for i=1:n
-		k1(i,:)=f(t(i),x(i,:),par);
-		k2(i,:)=f(t(i)+h,x(i,:)+h*k1(i,:),par);
-		x(i+1,:)=x(i,:)+(h/2)*(k1(i,:)+k2(i,:))
+		k1(i,:)=f(t(i),x(i,:),par(i));
+		k2(i,:)=f(t(i)+h,x(i,:)+h*k1(i,:),par(i));
+		x(i+1,:)=x(i,:)+(h/2)*(k1(i,:)+k2(i,:));
 	end
 end
 
